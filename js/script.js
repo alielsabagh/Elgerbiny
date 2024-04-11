4159bbcb53a7256d893dff562d8745da569cd648
ali_elsabagh <114312087+alielsabagh@users.noreply.github.com>
2024-04-12 01:30:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -232,3 +232,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault();
+});
